668f44f76e25ab826b97deb2600dc9514101962a
Maryem <mariammabtool@gmail.com>
2025-03-03 16:20:52 +0500

    Add inputbox

---
+++ tests/inputbox.spec.js
@@ -0,0 +1,10 @@
+const {test, expect} = require('@playwright/test')
+
+test('handle inoutbox', async ({page})=>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+
+    //inputbox - first name
+
+    
+})
