7c33efb7d09ca72538567efd64040a9cfb5f8308
Maryem <mariammabtool@gmail.com>
2025-04-16 11:13:45 +0500

    ..

--- tests/multiSelectDropdown.spec.js
+++ tests/multiSelectDropdown.spec.js
@@ -5,17 +5,17 @@ test("handle dropdown", async ({page})=>{
     await page.goto("https://testautomationpractice.blogspot.com/")
 
     //select multiple options from multi select dropdown
-    //await page.selectOption('#colors',['Blue','Red','Yellow'])
+    await page.selectOption('#colors',['Blue','Red','Yellow'])
 
     //Assertions
     //1) check number of options in dropdown
     //const options = await page.locator('#colors option')
-    //await expect(options).toHaveCount(5)
+    await expect(options).toHaveCount(5)
 
     //2) check number of options in dropdown using array
-    //const options = await page.$$('#colors option')
-    //console.log('Number of options:', options.length)
-    //await expect(options.length).toBe(5)
+    const options = await page.$$('#colors option')
+    console.log('Number of options:', options.length)
+    await expect(options.length).toBe(5)
 
     //3) check presence of value in dropdown
     const content = await page.locator('#colors').textContent()
