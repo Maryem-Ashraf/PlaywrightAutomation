46e157870614c8eccd24d8b3e4e9d0dc857466f6
Maryem <mariammabtool@gmail.com>
2025-04-28 15:45:44 +0500

    mouse hover

---
+++ tests/mouseHover.spec.js
@@ -0,0 +1,13 @@
+const {test, expect} = require('@playwright/test')
+
+test("Mouse Hover", async ({page})=>{ 
+    await page.goto("https://demo.opencart.com/")
+    const desktop = await page.locator("//a[normalize-space()='Desktops']")
+    const mac = await page.locator("//a[normalize-space()='Mac (1)']")
+
+    //mouse hover
+    await desktop.hover()
+    await mac.hover()
+
+
+})
